5fce2803eee7e3ddd94854aa9b00ad591c6c6a5b
SAMSUNG <SAMSUNG@DESKTOP-VDP83VP>
2020-04-13 13:52:06 +0900

    ch03-3-2.cpp 추가

--- ch03/ch03-3-2.cpp
+++ ch03/ch03-3-2.cpp
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
-void main() {
+void main() 
+{
 	int i, n = 0, *ptr;
 	int sale[2][4] = { {63, 84, 140, 130},
-					  {157, 209, 251, 312} };
+		{157, 209, 251, 312} };
 	
 	ptr = &sale[0][0];
 
